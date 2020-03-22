37b3786c5ba018d7236aa79d8f9fefad4bb7c9aa
Nicole Li <Nicole.Li@chrobinson.com>
2020-03-22 20:16:34 +0800

    add Fizzbuzz method

---
+++ FizzbuzzKata/FizzbuzzKata/Fizzbuzz.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace FizzbuzzKata
+{
+    public class Fizzbuzz
+    {
+        public string FizzbuzzMethod(int enterNumber)
+        {
+            return enterNumber.ToString();
+        }
+
+        static void Main(string[] args)
+        {
+            Fizzbuzz _fizzbuzz = new Fizzbuzz();
+            for (int i = 1; i <= 100; i++)
+            {
+                Console.WriteLine(_fizzbuzz.FizzbuzzMethod(i));
+            }
+        }
+    }
+}
